2d602f14482caea84c7f16c178470c97da74101f
JoshLee0915 <joshLee0915@gmail.com>
2016-02-11 00:28:11 -0700

    Implemented socket construction, binding, sending, threading, and util functions

--- NetworkLib/SocketLib/udp_sock.cpp
+++ NetworkLib/SocketLib/udp_sock.cpp
@@ -2,65 +2,198 @@
 
 using namespace Sockets;
 
+int UDP_Sock::getPort()
+{
+	return port;
+}
+
+int UDP_Sock::getTimeout()
+{
+	return timeout;
+}
+
+bool UDP_Sock::getMulticast()
+{
+	return multicast;
+}
+
 exception UDP_Sock::getLastException (  )
 {
-	throw NotImplementedException;
+	return lastException;
 }
 
 void UDP_Sock::clearError (  )
 {
-	throw NotImplementedException;
+	delete lastException;
+	lastException = 0;
 }
 
-UDP_Sock::UDP_Sock ( int port, bool multicast )
+UDP_Sock::UDP_Sock ( int port, bool multicast)
 {
+	UDP_Sock(port, DEFAULT_TIMEOUT, multicast, addressFamily);
 }
 
-UDP_Sock::UDP_Sock ( int port, int timeout, bool multicast )
+UDP_Sock::UDP_Sock ( int port, int timeout, bool multicast)
 {
+	// set up the defualt interface to bind to
+	sockaddr_in defualtInterface;
+	memset((char*)&defualtInterface, 0, sizeof(defualtInterface));
+	defualtInterface.sin_family = ADDR_FAMILY;
+	defualtInterface.sin_addr.s_addr = htonl(INADDR_ANY);
+	defualtInterface.sin_port = htons(port);
+
+
+	UDP_Sock(port, timeout, multicast, &defualtInterface);
+}
+
+UDP_Sock::UDP_Sock(int port, int timeout, bool multicast, sockaddr_in& sctInterface)
+{
+	// set the timeout
+	timeval timeoutVal;
+	timeoutVal.tv_sec = 0;
+	timeoutVal.tv_usec = timeout*1000;
+
+	// set the attrabutes
+	this->port = port;
+	this->timeout = timeout;
+	this->multicast = multicast;
+	this->rx = false;
+	this->rcvThread = 0;
+
+	// open the socket
+	if ((sct = socket(ADDR_FAMILY, SOCK_DGRAM, 0)) < 0)
+		throw errorNumToException(errno);
+
+	// check if the timeout should be set
+	if(timeout > 0)
+		// set the receive timeout
+		if (setsockopt(sct, SOL_SOCKET, SO_RCVTIMEO, &timeoutVal, sizeof(timeoutVal)) < 0)
+			throw errorNumToException(errno);	// timeout failed to set
+
+	// bind the socket
+	if (bind(sct, (sockaddr*)&sctInterface, sizeof(sctInterface)) < 0)
+		throw errorNumToException(errno);
 }
 
 UDP_Sock::UDP_Sock ( const UDP_Sock& socket )
 {
+	UDP_Sock(socket.port, socket.timeout, socket.multicast);
 }
 
 UDP_Sock::~UDP_Sock (  )
 {
+	close();
 }
 
 void UDP_Sock::close (  )
 {
+	stopReceive();	// first stop receiving messages
+
+	// close the socket
 	throw NotImplementedException;
 }
 
 bool UDP_Sock::connect ( string addr )
 {
+	defaultAddr = addr;
+
 	throw NotImplementedException;
 }
 
 bool UDP_Sock::send ( char* msg )
 {
-	throw NotImplementedException;
+	return send(msg, defaultAddr);
 }
 
 bool UDP_Sock::send ( char* msg, string ip )
 {
-	throw NotImplementedException;
+	// set up the basic address
+	sockaddr_in dst;
+	memset((char*)&dst, 0, sizeof(dst));
+	dst.sin_family = ADDR_FAMILY;
+	dst.sin_port = htons(port);
+
+	// translate the ip string to the binary format
+	if (inet_pton(ADDR_FAMILY, ip.c_str, &(dst.sin_addr) < 0)
+	{
+		lastException = errorNumToException(errno);
+		return false;	// failed
+	}
+
+	return send(msg, &dst);
 }
 
-UDP_Sock::send ( char* msg, const struct sockaddr_in& addr )
+bool UDP_Sock::send ( char* msg, const struct sockaddr_in& addr )
 {
-	throw NotImplementedException;
+	if (sendto(sct, msg, sizeof(msg), 0, (sockaddr*)addr, sizeof(addr)) < 0)
+	{
+		lastException = errorNumToException(errno);
+		return false;	// failed
+	}
+
+	return true;
 }
 
 bool UDP_Sock::startReceive ( void (*handler)(struct sockaddr_in&, char*) )
 {
-	throw NotImplementedException;
+	// check if the sock is already receiving
+	if (rx == true)
+	{
+		lastException = new exception("Receive is already running");
+		return false;
+	}
+
+	try
+	{
+		// set to transmit
+		rx = true;
+		// create the thread
+		rcvThread = new thread(receive,handler);
+	}
+	catch (exception& e)
+	{
+		rx = false;
+
+		if (lastException != 0)
+			clearError();
+
+		lastException = e;
+		return false;
+	}
+
+	return true;
 }
 
 bool UDP_Sock::stopReceive (  )
 {
-	throw NotImplementedException;
+	// check if receive has been started
+	if (!rx && rcvThread == 0)
+		return true;	// already stopped so return true
+
+	// set the receive flag to false to stop receiving
+	rx = false;
+
+	try
+	{
+		// wait for the timeout*2 to allow the thread to exit gracefully
+		std::this_thread::sleep_for(std::chrono::milliseconds(timeout * 2));
+
+		// delete the thread
+		if(rcvThread!=0)
+			delete rcvThread;
+
+		rcvThread = 0;
+	}
+	catch (exception& e)
+	{
+		if (lastException != 0)
+			clearError();
+
+		lastException = e;
+		return false;
+	}
+
+	return true;
 }
 
 char* UDP_Sock::receive (  )
@@ -70,6 +203,32 @@ char* UDP_Sock::receive (  )
 
 void UDP_Sock::receive ( void (*handler)(struct sockaddr_in, char*) )
 {
-	throw NotImplementedException;
+	while (rx)
+	{
+	}
 }
 
+exception UDP_Sock::errorNumToException(int err)
+{
+	switch (err)
+	{
+	case EAFNOSUPPORT:
+		return exception("Specfied address family is not suported");
+	case EMFILE:
+		return exception("Process has exhausted file descriptors");
+	case ENFILE:
+		return exception("System has exhausted file descriptors");
+	case EPROTONOSUPPORT:
+		return exception("Specfied address family is not suport the specfied protocol");
+	case EPROTOTYPE:
+		return exception("Specfied protocol does not support specfied socket type");
+	case EACCES:
+		return exception("Process has insufficient privileges");
+	case ENOBUFS:
+		return exception("Insuffucuent resources availble");
+	case ENOMEM:
+		return exception("Insuffucuent memory availble");
+	default:
+		return exception("An unknown error has occured");
+	}
+}
--- NetworkLib/SocketLib/udp_sock.h
+++ NetworkLib/SocketLib/udp_sock.h
@@ -1,40 +1,58 @@
 #ifndef UDP_SOCK_H
 #define UDP_SOCK_H
 
+#include <thread>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <errno.h>
+
 #include "networkinterface.h"
 #include "notimplementedexception.h"
 
+#define ADDR_FAMILY AF_INET
+#define DEFAULT_TIMEOUT 1000 // 1 second timeout by default
+
+using std::thread;
+
 namespace Sockets
 {
-	class UDP_Sock: public NetworkInterface {
-	  // Associations
-	  // Attributes
-	  private:
-		int port;
-		struct sockaddr_in defaultAddr;
-		int sct;
-		exception lastException;
-		pthread_t  rcvThread;
-		void  (*handler)(struct sockaddr_in&, char*);
+	class UDP_Sock : public NetworkInterface {
+		// Associations
+		// Attributes
+	private:
+		int port, timeout, sct;
+		bool multicast;
+		sockaddr_in defaultAddr;
+		exception* lastException;
+		thread*  rcvThread;
 		volatile bool rx;
-	  // Operations
-	  public:
-		exception getLastException (  );
-		void clearError (  );
-		UDP_Sock ( int port, bool multicast );
-		UDP_Sock ( int port, int timeout, bool multicast );
-		UDP_Sock ( const UDP_Sock& socket );
-		~UDP_Sock (  );
-		void close (  );
-		bool connect ( string addr );
-		bool send ( char* msg );
-		bool send ( char* msg, string ip );
-		send ( char* msg, const struct sockaddr_in& addr );
-		bool startReceive ( void (*handler)(struct sockaddr_in&, char*) );
-		bool stopReceive (  );
-		char* receive (  );
-	  protected:
-		void receive ( void (*handler)(struct sockaddr_in, char*) );
+		// Operations
+	public:
+		int getPort();
+		int getTimeout();
+		bool getMulticast();
+		exception getLastException();
+		void clearError();
+		UDP_Sock(int port, bool multicast);
+		UDP_Sock(int port, int timeout, bool multicast);
+		UDP_Sock(int port, int timeout, bool multicast, sockaddr_in& sctInterface);
+		UDP_Sock(const UDP_Sock& socket);
+		~UDP_Sock();
+		void close();
+		bool connect(string addr);
+		bool send(char* msg);
+		bool send(char* msg, string ip);
+		bool send(char* msg, const struct sockaddr_in& addr);
+		bool startReceive(void(*handler)(struct sockaddr_in&, char*));
+		bool stopReceive();
+		char* receive();
+	protected:
+		void receive(void(*handler)(struct sockaddr_in, char*));
+	private:
+		exception errorNumToException(int err);
 	};
 }
 #endif

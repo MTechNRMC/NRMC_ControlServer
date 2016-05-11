ed01494db782ff8f67e04df4b913a15f1bb64c26
Josh Lee <joshlee0915@gmail.com>
2016-05-11 13:52:36 -0600

    Added the periperal system enum

---
+++ HardwareLib/peripheralsystem.h
@@ -0,0 +1,16 @@
+#ifndef PERIPHERALSYSTEM_H
+#define PERIPHERALSYSTEM_H
+
+namespace NRMCHardware
+{
+	// Enum
+	enum PeripheralSystem
+	{
+		LocomotionSystem,
+		MSP430SensorSystem,
+		ObsticalDetectionLIDAR,
+		LocalizationLIDAR
+	};
+}
+
+#endif
